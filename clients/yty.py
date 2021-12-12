92f73243ef98c654614b5310f3d75c520718ef2f
Dmml0621 <35722335+Dmml0621@users.noreply.github.com>
2021-12-12 17:25:45 -0500

    latest no polyfit error

--- clients/yty.py
+++ clients/yty.py
@@ -8,32 +8,34 @@ import matplotlib.pyplot as plt
 import numpy as npb
 import Robogame as rg
 
-def plot_scatter(type, df):
-	scatter_plot = alt.Chart(df).mark_circle(color="black", opacity=0.1).transform_filter(
-	    alt.FieldEqualPredicate(field='column', equal=f"{type}")
-	).transform_filter(
+def plot_scatter(df,type):
+	scatter_plot = alt.Chart(df).mark_circle(color="black", opacity=0.2).transform_filter(
 	    alt.datum.winner != -2
 	).encode(
-	    x=alt.X("value:Q", title=f"{type}"),
+	    x=alt.X("value:Q", title=type),
 	    y=alt.Y("Productivity:Q")
 	)
+
 	def fit_curve(df):
-		if len(df) == 0:
-			return 
+		if len(df)==0:
+			return []
+		if (df['value'].isnull().any() and df['Productivity'].isnull().any()): return []
+		print('fit',df)
 		max_value = df['value'].max()
 		min_value = df['value'].min()
-		fit = np.polyfit(df['value'].astype(float), df['Productivity'].astype(float), 1)
+		## judge whether value is float
+		fit = np.polyfit(df['value'].astype(str).astype(float), df['Productivity'].astype(str).astype(float), 1)
 		x = np.linspace(min_value,max_value,100)
 		func = np.poly1d(fit)
 		res = []
-
+				
 		for i in np.arange(0,len(x)):
 			res.append({'column':df['column'], 'value':x[i], 'Productivity':func(x[i])})
 		return res
 
-	fit_df = df.loc[(df['column'] == type)&(df['winner'] != -2)]
+	fit_df=df[df.winner!=-2]
 	res = fit_curve(fit_df)
-
+	
 	fit_plot = alt.Chart(alt.Data(values=res)).mark_line().encode(
 		x='value:Q',
 		y='Productivity:Q'
@@ -45,14 +47,39 @@ def plot_scatter(type, df):
 	)
 
 def scatter_charts(robots, hints):
-    parthints_df = pd.read_json(json.dumps(hints), orient='records')
-    if len(parthints_df) == 0:
-        return
-    part_df = pd.merge(parthints_df, robots, how='left', on='id')
-    plot_type=['Repulsorlift Motor HP', 'Astrogation Buffer Length', 'Polarity Sinks', 'AutoTerrain Tread Count', 'InfoCore Size', 'Sonoreceptors', 'Cranial Uplink Bandwidth']
+	parthints_df = pd.read_json(json.dumps(hints), orient='records')
+	if len(parthints_df) == 0:
+		return
+	part_df = pd.merge(parthints_df, robots, how='left', on='id')
+	plot_type_q=[ 'Polarity Sinks', 'AutoTerrain Tread Count',  'Sonoreceptors', 'Cranial Uplink Bandwidth','Repulsorlift Motor HP', 'Astrogation Buffer Length','InfoCore Size']
+	plot_list = []
+	for type in plot_type_q:
+		type_df=part_df[part_df.column==type]
+		plot_list.append(plot_scatter(type_df,type))
 
-    plot_list = []
-    for type in plot_type:
-        plot_list.append(plot_scatter(type, part_df))
+	plot_type_n=[ 'Axial Piston Model','Nanochip Model','Arakyd Vocabulator Model']
 
-    return (plot_list[0] | plot_list[1]) & (plot_list[2] | plot_list[3]) & (plot_list[4] | plot_list[5]) & plot_list[6] 
+	for type in plot_type_n:
+		type_df=part_df[part_df.column==type]
+		plot_list.append(plot_bar(type_df,type))   
+	
+
+	return (plot_list[0] | plot_list[1] | plot_list[2] | plot_list[3] |plot_list[4]) & ( plot_list[5] | plot_list[6] | plot_list[7] | plot_list[8] | plot_list[9]) 
+
+
+def plot_bar(df,type):
+	scatter = alt.Chart(df).mark_circle(opacity=0.4,color='black').transform_filter(
+	    alt.datum.winner!=-2
+	).encode(
+	    x=alt.X("value:N",title=type),
+	    y=alt.Y("Productivity:Q")
+	)
+	
+	bar = alt.Chart(df).mark_bar(opacity=0.3).transform_filter(
+	    alt.datum.winner!=-2
+	).encode(
+	    x=alt.X("value:N"),
+	    y=alt.Y("mean(Productivity):Q"),
+	    tooltip=['value:N','mean(Productivity)']
+	)
+	return scatter+bar
