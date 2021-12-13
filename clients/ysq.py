e5c981f1919276fd6e26eecc0fc4bd3122d816d5
Dmml0621 <35722335+Dmml0621@users.noreply.github.com>
2021-12-12 22:48:42 -0500

    pre_final version

--- clients/ysq.py
+++ clients/ysq.py
@@ -8,23 +8,21 @@ import networkx as nx
 import nx_altair as nxa
 
 #新增关于productivity和time的dataframe
-def generate_df(robots, cur_time):
-    data_list = []  #这个不要放在循环里
+
+def plot_productivity_time(robots, cur_time, data_list):
     item1 = {'team': "team 1", 'time': cur_time, 'productivity': robots[robots.winner == 1]['Productivity'].sum()}
     item2 = {'team': "team 2", 'time': cur_time, 'productivity': robots[robots.winner == 2]['Productivity'].sum()}
     data_list.append(item1)
     data_list.append(item2)
-    return pd.DataFrame(data_list)
+    df = pd.DataFrame(data_list)
 
-def plot_productivity_time(robots, cur_time):
-    df = generate_df(robots, cur_time)
     nearest = alt.selection(type='single', nearest=True, on='mouseover',
                             fields=['time'], empty='none')
     line = alt.Chart(df).mark_line(interpolate='basis').encode(
         x='time:Q',
         y='productivity:Q',
         color='id:N'
-    )
+    ).properties(width=600)
 
     selectors = alt.Chart(df).mark_point().encode(
         x='time:Q',
@@ -53,22 +51,22 @@ def plot_winner(robots):
     team_1_productivity = robots[robots.winner == 1]['Productivity'].sum()
     team_2_productivity = robots[robots.winner == 2]['Productivity'].sum()
     data_source = pd.DataFrame({"team": ['team1', 'team2'], "productivity": [team_1_productivity, team_2_productivity]})
-    base = alt.Chart(data_source).properties()
+    base = alt.Chart(data_source)
     bar = base.mark_bar().encode(
-        x= alt.X('sum(productivity)',scale=alt.Scale(domain=[0, team_1_productivity+team_2_productivity])),
+        x=alt.X('sum(productivity)',scale=alt.Scale(domain=[0, team_1_productivity+team_2_productivity]), title='Winner'),
         color='team',
         tooltip = ['team', 'productivity']
-    )
+    ).properties(width=600)
     # pie = base.mark_area().encode(
     #     theta=alt.Theta(field="productivity", type="quantitative"),
     #     color=alt.Color(field="team", type="nominal"),
     #     tooltip = ['team', 'productivity']
     # )
     rule = base.mark_rule(color='black').encode(
-        x='median(productivity)',
+        x=alt.X('median(productivity)', title='Winner'),
         size=alt.value(3)
     )
     return (bar + rule)
 
-def productivity_winner(robots, cur_time):
-    return alt.vconcat(plot_productivity_time(robots, cur_time), plot_winner(robots))
+def productivity_winner(robots, cur_time, data_list):
+    return alt.vconcat(plot_productivity_time(robots, cur_time, data_list), plot_winner(robots))
